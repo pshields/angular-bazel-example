4a1dc532a95a48346836f86ad116807cf4c9f078
Patrick Shields <pshields@gmail.com>
2018-01-20 13:28:41 -0600

    Repro of Angular Material Bazel compilation issue

--- src/hello-world/hello-world.module.ts
+++ src/hello-world/hello-world.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
-
+import {MatButtonModule} from '@angular/material';
 import {HelloWorldComponent} from './hello-world.component';
 
 @NgModule({
   declarations: [HelloWorldComponent],
+  imports: [MatButtonModule],
   exports: [HelloWorldComponent],
 })
 export class HelloWorldModule {}
